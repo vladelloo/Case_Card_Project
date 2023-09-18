c054c46345c169ccc5639b8236e37d7255650cec
vladelloo <vladel199@yandex.ru>
2023-09-18 22:35:07 +0300

    Начальная страница, страница с куаркодом, страница - база куаркодов.

---
+++ qr_generator/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import hashlib
+# Create your views here.
+qr_list_base = []
+
+def home(request):
+    return render(request, 'qr_generator/home.html')
+
+def qr_code(request, base=qr_list_base):
+
+    open_key = list(request.GET.get('open key'))
+
+    if request.GET.get('tourniquet'):
+        open_key.extend(str('tourniquet'))
+    if request.GET.get('anesthesia'):
+        open_key.extend(str('anesthesia'))
+    if request.GET.get('antibiotic'):
+        open_key.extend(str('antibiotic'))
+    if request.GET.get('tranexamic acid'):
+        open_key.extend(str('tranexamic acid'))
+    if request.GET.get('antiemetic'):
+        open_key.extend(str('antiemetic'))
+    if request.GET.get('immobilization'):
+        open_key.extend(str('immobilization'))
+
+    qr_list = []
+    for i in open_key:
+        qr_list += [i.encode()]
+
+    hash_dabl = []
+    for i in qr_list:
+        hash_dabl += [hashlib.sha256(i).hexdigest()]
+
+    hash_dabl = "".join(hash_dabl)
+
+    qrcode = hashlib.sha256((hash_dabl).encode()).hexdigest()
+
+    base.append(str(qrcode))
+
+    return render(request, 'qr_generator/qr_code.html', {'qrcode': qrcode})
+
+def magazine(request):
+    return render(request, 'qr_magazine/magazine.html', {'base': qr_list_base})
---
+++ qr_magazine/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QrMagazineConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'qr_magazine'
