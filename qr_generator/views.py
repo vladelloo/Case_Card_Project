e90201dd7f4b1764cabb58b17532215a778e5086
vladelloo <vladel199@yandex.ru>
2023-09-26 15:41:54 +0300

    Версия 1.0 загружаю сайт в сеть

--- qr_generator/views.py
+++ qr_generator/views.py
@@ -1,3 +1,4 @@
+import qrcode
 from django.shortcuts import render
 from django.http import HttpResponse
 import hashlib
@@ -9,23 +10,14 @@ def home(request):
 
 def qr_code(request):
 
-    open_key = list(request.GET.get('open key'))
+    names_list = ['nickname', 'birthday', 'personalNumber', 'subdivision', 'diagnosis', 'tourniquet',
+                  'anesthesia', 'allergy', 'note', 'infuzion', 'shock', 'blood', 'bloodRh', 'ultrasound', 'xRay', 'sorting']
 
-    if request.GET.get('tourniquet'):
-        open_key.extend(str('tourniquet'))
-    if request.GET.get('anesthesia'):
-        open_key.extend(str('anesthesia'))
-    if request.GET.get('antibiotic'):
-        open_key.extend(str('antibiotic'))
-    if request.GET.get('tranexamic acid'):
-        open_key.extend(str('tranexamic acid'))
-    if request.GET.get('antiemetic'):
-        open_key.extend(str('antiemetic'))
-    if request.GET.get('immobilization'):
-        open_key.extend(str('immobilization'))
+    description = request.GET.get('firstName')
+    description = [description + request.GET.get(name) for name in names_list if request.GET.get(name)]
 
     qr_list = []
-    for i in open_key:
+    for i in description:
         qr_list += [i.encode()]
 
     hash_dabl = []
@@ -34,6 +26,9 @@ def qr_code(request):
 
     hash_dabl = "".join(hash_dabl)
 
-    qrcode = hashlib.sha256((hash_dabl).encode()).hexdigest()
+    data = hashlib.sha256((hash_dabl).encode()).hexdigest()
+    qrcode_img = qrcode.make(data)
+    qrcode_img.save(f'D:/Case_Card/media/qr_magazine/qr_base/{hash_dabl[:5]}.jpg')
+    img_address = str(f'D:/Case_Card/media/qr_magazine/qr_base/{hash_dabl[:5]}.jpg')
 
-    return render(request, 'qr_generator/qr_code.html', {'qrcode': qrcode})
+    return render(request, 'qr_generator/qr_code.html', {'qrcode': img_address})
--- qr_magazine/models.py
+++ qr_magazine/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class QR_base(models.Model):
+
     title = models.CharField(max_length=250)
     qrcode = models.ImageField(upload_to='qr_magazine/qr_base/')
     url = models.URLField(blank=True)
